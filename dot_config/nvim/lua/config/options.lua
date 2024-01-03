3848edbbb0c4eae01234b3ccec66b1436e79ed88
Drew <drew@drewtopia.com.au>
2024-01-04 09:42:54 +1100

    Updated zshrc config

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.wrap = false
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.scrolloff = 999
